2d47db0f7ccbb769f082c3e1b901bd07c6d22c67
HaileyStorm <55606304+HaileyStorm@users.noreply.github.com>
2021-06-03 20:12:43 -0600

    Update unbounded.go

--- mailbox/unbounded.go
+++ mailbox/unbounded.go
@@ -1,8 +1,8 @@
 package mailbox
 
 import (
-	"github.com/AsynkronIT/protoactor-go/internal/queue/goring"
-	"github.com/AsynkronIT/protoactor-go/internal/queue/mpsc"
+	"github.com/AsynkronIT/protoactor-go/internals/queue/goring"
+	"github.com/AsynkronIT/protoactor-go/internals/queue/mpsc"
 )
 
 type unboundedMailboxQueue struct {
